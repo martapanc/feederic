934adf12f7634952d8ec6b351d5bd16ac1b0ae29
Marta Pancaldi <marta.panc@gmail.com>
2020-02-10 19:00:19 +0000

    Initial commit

---
+++ src/main/java/com/example/feederic/FeedericApplication.java
@@ -0,0 +1,13 @@
+package com.example.feederic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedericApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeedericApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/feederic/FeedericApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.feederic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedericApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
